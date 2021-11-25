2776392069638f07bac484b8fe3166e7ef3834f7
high-Cy <loh.chengyin@gmail.com>
2021-11-26 01:34:32 +1100

    level data

---
+++ levels/item.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="item" tilewidth="32" tileheight="32" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../assets/items/diamond.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../assets/items/diamond2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../assets/items/health_potion.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../assets/items/doub_jump_potion.png"/>
+ </tile>
+</tileset>
---
+++ levels/startend.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="startend" tilewidth="32" tileheight="32" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../assets/terrain/start_end/start.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../assets/terrain/start_end/end.png"/>
+ </tile>
+</tileset>
